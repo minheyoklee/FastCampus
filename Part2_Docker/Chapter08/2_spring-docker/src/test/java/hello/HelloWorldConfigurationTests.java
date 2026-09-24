9d6c427a4d8f37694d97c893c27c6f4b0c422855
Minhyeok <funckyou0818@gmail.com>
2026-09-25 01:09:41 +0900

    jenkins

--- Part2_Docker/Chapter08/2_spring-docker/src/test/java/hello/HelloWorldConfigurationTests.java
+++ Part2_Docker/Chapter08/2_spring-docker/src/test/java/hello/HelloWorldConfigurationTests.java
@@ -23,14 +23,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 @DirtiesContext
 public class HelloWorldConfigurationTests {
 
